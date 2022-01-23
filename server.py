13f0a3001dba96e3862717b54384ab36f963150c
plche <che.percy.l@gmail.com>
2022-01-22 21:40:36 -0500

    Added session

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+
+# establecemos una clave secreta para que nuestra app pueda usar sesiones
+app.secret_key = '0ad3cc8dfb44979d50f601f835c891571b8215006b025f3cb8167791f49c4060'
 
 # nuestra ruta de índice manejará la representación de nuestro formulario
 @app.route('/')
@@ -10,9 +13,17 @@ def index():
 def create_user():
     print("Got Post Info")
     print(request.form)
+    # aquí agregamos dos propiedades a la sesión para almacenar el nombre y el correo electrónico
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
     # Nunca renderices una plantilla en una solicitud POST
     # En su lugar, redirigiremos a nuestra ruta de índice
-    return redirect('/')
+    return redirect("/show")
+
+@app.route("/show")
+def show_user():
+    print("Showing the User Info From the Form")
+    return render_template("show.html", name_on_template=session['username'], email_on_template=session['useremail'])
 
 if __name__ == "__main__":
     app.run(debug=True)
